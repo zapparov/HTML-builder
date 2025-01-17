12994d38086a093773baed115091883eef91e315
Ildar Zapparov <izapparov27@gmail.com>
2025-01-17 12:56:51 +0500

    feat: finish write-file subtask

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'output.txt');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log(
+  'Введите текст для записи в файл. Напишите "exit" или нажмите ctrl + c для выхода.',
+);
+
+rl.on('line', (input) => {
+  if (input.trim().toLowerCase() === 'exit') {
+    console.log('Пока!');
+    rl.close();
+  } else {
+    fs.appendFile(filePath, input + '\n', (err) => {
+      if (err) throw err;
+      console.log(
+        'Текст добавлен в файл. Введите еще текст или напишите "exit" для выхода.',
+      );
+    });
+  }
+});
+
+rl.on('close', () => {
+  process.exit(0);
+});
